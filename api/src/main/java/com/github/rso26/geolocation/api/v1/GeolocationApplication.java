f9dd0face3382edb9c56f2a87e1fe84f68c3d646
Aleksander Tomic <alexander-tomic@outlook.com>
2019-12-02 20:36:16 +0100

    Added discovery logic and basic k8s deployment.

--- api/src/main/java/com/github/rso26/geolocation/api/v1/GeolocationApplication.java
+++ api/src/main/java/com/github/rso26/geolocation/api/v1/GeolocationApplication.java
@@ -1,5 +1,6 @@
 package com.github.rso26.geolocation.api.v1;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
@@ -7,6 +8,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@RegisterService
 @ApplicationPath("/v1")
 @OpenAPIDefinition(info = @Info(title = "GeolocationApi", version = "v1.0.0"), servers = @Server(url = "http://localhost:8084/v1"))
 public class GeolocationApplication extends Application {
